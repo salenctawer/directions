936b586be6bd65b49b8f9ece5bad340ec52674f0
salenctawer <salenctawer@gmail.com>
2022-06-26 19:41:59 +0500

    refactoring bookmarks

--- src/Components/Home/Sidebar/Bookmarks/Table/BookmarksList.jsx
+++ src/Components/Home/Sidebar/Bookmarks/Table/BookmarksList.jsx
@@ -1,122 +1,40 @@
 // *** NPM ***
 import React, { useEffect, useState } from 'react'
-import { Table, Select } from 'antd';
-
 import styles from './Table.module.scss'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
 
-const TableSelect = (props) => {
-    const { defaultValue, elementId, dataIndex,  refreshMapMarkers, changeDeliveryPoint, locationVariants } = props
-    const { Option } = Select
-
-
-    const tableSelectHandler = (pointId) => {
-        const newDeliveryPoint = locationVariants.find(item => item.id === pointId)
-        changeDeliveryPoint({
-            elementId,
-            dataIndex,
-            newDeliveryPoint,
-            refreshMapMarkers
-        })
-    }
-    
-    return(
-        <Select 
-            defaultValue={defaultValue} 
-            style={{ width: 100 }} 
-            onChange={tableSelectHandler} 
-            onClick={e => e.stopPropagation()}
-        >
-            {locationVariants?.map((location) => {
-                return(
-                    <Option key={location.id} value={location.id}>{location.name}</Option>
-                )
-            })}
-      </Select>
-    )
-}
 
 
 const BookmarksList = (props) => {
-    const { orders, setCurrentActiveLocation, changeDeliveryPoint,  locationVariants } = props
-    const [isRowActive, setRowActive] = useState(null)
-
-
-    const columns = [
-        {
-            title: 'Location from',
-            dataIndex: 'pick',
-            key: 'pick',
-            width: 200,
-            render: (text, record) => {
-                return(
-                    <TableSelect
-                        locationVariants={locationVariants} 
-                        defaultValue={record.pick.name}
-                        elementId={record.id}
-                        dataIndex={'pick'}
-                        changeDeliveryPoint={changeDeliveryPoint}
-                        refreshMapMarkers={isRowActive === record.id ? true : false}
-                    />
-                )
-            },
-        },
-        {
-            title: 'Location to',
-            dataIndex: 'deliver',
-            key: 'deliver',
-            width: 200,
-            render: (text, record) => {
-                return(                    
-                    <TableSelect
-                        locationVariants={locationVariants} 
-                        defaultValue={record.deliver.name}
-                        elementId={record.id} 
-                        dataIndex={'deliver'}
-                        changeDeliveryPoint={changeDeliveryPoint}
-                        refreshMapMarkers={isRowActive === record.id ? true : false}
-                    />
-                )
-                
-            },
-        },
-        {
-            title: 'Vehicle',
-            dataIndex: 'vehicle',
-            key: 'vehicle',
-            width: 200,
-            minWidth: 500,
-        },
-        {
-            title: 'Price',
-            dataIndex: 'price',
-            key: 'price',
-            width: 100,
-        },
-    ]
     
-    const clickHandler = (event, tableRow) => {
-        setCurrentActiveLocation({
-            pick_position: tableRow.pick.position,
-            deliver_position: tableRow.deliver.position,            
-        })
-        setRowActive(tableRow.id)
-    }
+    console.log(props)
 
+    const [dense, setDense] = React.useState(false);
+    const [secondary, setSecondary] = React.useState(false);
 
     return(
         <section className={styles.orderTable}>
-            <Table
-                dataSource={orders}
-                columns={columns}
-                onRow={(record, rowIndex) => {
-                    return {
-                        onClick: event => clickHandler(event, record),
-                    };
-                }}
-                scroll={{ x:'max-content' }}
-                rowClassName={(record, index) => record.id === isRowActive? styles.activeRow :  ''}
-            />            
+            <Grid item xs={12} md={6}>
+                <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+                    Закладки
+                </Typography>
+                <List dense={dense}>
+                    {props.orders?.map(item=>
+                            <ListItem>
+                            <ListItemText
+                                primary={item.deliver.name}
+                                secondary={secondary ? 'Secondary text' : null}
+                            />
+                            </ListItem> 
+                        )
+                    }
+                </List>
+            </Grid>            
         </section>
     )
 }
