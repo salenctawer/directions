5612da18514fe160cba5379f038ad8c2de6667e1
salenctawer <salenctawer@gmail.com>
2022-06-27 20:16:48 +0500

    fixing styles

--- src/Components/Home/Sidebar/Bookmarks/Table/BookmarksList.jsx
+++ src/Components/Home/Sidebar/Bookmarks/Table/BookmarksList.jsx
@@ -1,13 +1,10 @@
 // *** NPM ***
 import React, { useEffect, useState } from 'react'
 import styles from './Table.module.scss'
-import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import { Table, Select } from 'antd';
-
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import Button from '@mui/material/Button';
 
 
 
@@ -26,20 +23,35 @@ const BookmarksList = (props) => {
 
     return(
         <section className={styles.orderTable}>
+            <div className={styles.titlesContainer}>
+                <span>
+                    От куда
+                </span>
+                <span>
+                    Куда
+                </span>
+            </div>
+
             {
                 orders?.map(item=>
                     <div className={styles.row}>
-                        <ListItem onClick={clickHandler(item)}>
-                            <ListItemText
-                            primary={item.pick.name}
-                            />
-                            <ListItemText
-                            primary={item.deliver.name}
-                            />
+                        <ListItem onClick={clickHandler(item)} className={styles.listItem}>
+                            <div>
+                                <ListItemText
+                                primary={item.pick.name}
+                                />
+                            </div>
+                            <ArrowForwardIcon />
+                            <div>
+                                <ListItemText
+                                primary={item.deliver.name}
+                                />
+                            </div>
                         </ListItem>  
                     </div>
                 )
-            }           
+            }
+            <Button variant="contained" style={{marginTop:'600px'}}>Создать маршрут</Button>           
         </section>
     )
 }
